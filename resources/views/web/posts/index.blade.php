bb598e34f0f12eb8f004f751b190ca1c7e74b7e5
mkita <8792739+mkitada@users.noreply.github.com>
2022-09-23 18:35:24 +0900

    投稿一覧画面から投稿フォームを削除

--- resources/views/web/posts/index.blade.php
+++ resources/views/web/posts/index.blade.php
@@ -17,18 +17,5 @@
         <br><hr>
     @endforeach
 @endisset
-
-<h2>フォーム</h2>
-<form action="/create" method="POST">
-    名前:<br>
-    <input name="name">
-    <br>
-    コメント:<br>
-    <textarea name="comment" rows="4" cols="40"></textarea>
-    <br>
-    {{ csrf_field() }}
-    <button class="btn btn-success"> 送信 </button>
-</form>
-
 </body>
 </html>
